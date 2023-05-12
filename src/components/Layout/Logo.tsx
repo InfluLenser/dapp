3d38e0ccb522317acf8c4be65adb4ca493936995
KPomales <kirstenpomales@gmail.com>
2023-05-12 23:33:29 +0100

    Fixing logos.
    Yes, green. We like green.

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -2,17 +2,13 @@ import Link from 'next/link';
 
 function Logo() {
   return (
-    <h1 className='text-2xl text-il-green-600'>
+    <h1 className='text-2xl text-white'>
       <Link href='/'>
-<<<<<<< Updated upstream
-        Influ<span className='text-il-green-900'>Lenser</span>
-=======
       <img
           className=' rounded-full'
           alt=''
           src={'/images/logo-ln.webp'}      
         />
->>>>>>> Stashed changes
       </Link>
     </h1>
   );
