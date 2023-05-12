d1c32a2263b5f0612333d5fa1fac11eda80927ec
KPomales <kirstenpomales@gmail.com>
2023-05-12 23:19:50 +0100

    Adding logos!
    Yes, we love logos here. We love Lens.

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -4,10 +4,20 @@ function Logo() {
   return (
     <h1 className='text-2xl text-il-green-600'>
       <Link href='/'>
+<<<<<<< Updated upstream
         Influ<span className='text-il-green-900'>Lenser</span>
+=======
+      <img
+          className=' rounded-full'
+          alt=''
+          src={'/images/logo-ln.webp'}      
+        />
+>>>>>>> Stashed changes
       </Link>
     </h1>
   );
 }
 
 export default Logo;
+
+
