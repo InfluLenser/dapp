da1353de5f768737c9f4168dfd00a93bd9a70d1f
KPomales <kirstenpomales@gmail.com>
2023-05-13 21:31:05 +0100

    Hella frontend stuff, yea yea woo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,16 +12,10 @@ function Home() {
       </div>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">First Link</a>
-      <a className="mr-5 hover:text-gray-900">Second Link</a>
-      <a className="mr-5 hover:text-gray-900">Third Link</a>
-      <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+      <a className="mr-5 hover:text-gray-900">Dashboard</a>
+      <a className="mr-5 hover:text-gray-900">Get Sponsored</a>
+      <a className="mr-5 hover:text-gray-900">Find Influencers</a>
     </nav>
-    <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
   </div>
 </header>
 <section className="text-gray-700 body-font">
@@ -122,7 +116,7 @@ function Home() {
     <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
       <div className="flex flex-col mb-10 lg:items-start items-center">
         <div className="flex-grow">
-          <h2 className="text-gray-900 text-lg title-font font-medium mb-3">🌿 Lens Protocol Native 🌿</h2>
+          <h2 className="text-gray-900 text-2xl title-font font-medium mb-3">🌿 Lens Protocol Native Posts 🌿</h2>
           <p className="leading-relaxed text-base">Right after being hired for a sponsorship gig, InfluLENSers can immediatley post on Lens - all from their influencer dashboard.</p>
           <a className="mt-3 text-green-500 inline-flex items-center">Learn About Lens
           </a>
@@ -131,8 +125,8 @@ function Home() {
       <div className="flex flex-col mb-10 lg:items-start items-center">
 
         <div className="flex-grow">
-          <h2 className="text-gray-900 text-lg title-font font-medium mb-3">🍀 Hiring Powered by TalentLayer 🦝</h2>
-          <p className="leading-relaxed text-base">Your influencer marketing posts are broadcast to TalentLayer's network of interoperable hiring platforms.</p>
+          <h2 className="text-gray-900 text-2xl title-font font-medium mb-3">🍀 Hiring Powered by TalentLayer 🦝</h2>
+          <p className="leading-relaxed text-base">Have peace of mind with on-chain escrow and job reviews. Your influencer marketing posts are broadcast to TalentLayer's network of interoperable hiring platforms.</p>
           <a className="mt-3 text-green-500 inline-flex items-center">Learn About TalentLayer
           </a>
         </div>
@@ -147,40 +141,64 @@ function Home() {
     </div>
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/3 md:w-1/2 p-4">
-        <div className="border border-gray-300 p-6 rounded-lg">
-          
-          
-        </div>
-      </div>
-      <div className="xl:w-1/3 md:w-1/2 p-4">
-        <div className="border border-gray-300 p-6 rounded-lg">
-          
+        <div className="border p-6 rounded-lg">
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/polygon.png"></img>
           
         </div>
       </div>
       <div className="xl:w-1/3 md:w-1/2 p-4">
-        <div className="border border-gray-300 p-6 rounded-lg">
+        <div className="border p-6 rounded-lg">
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/lens.png"></img>
           
         </div>
       </div>
       <div className="xl:w-1/3 md:w-1/2 p-4">
-        <div className="border border-gray-300 p-6 rounded-lg">
-          
-          
+        <div className="border  p-6 rounded-lg">
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/talentlayer.png"></img>
         </div>
       </div>
       <div className="xl:w-1/3 md:w-1/2 p-4">
-        <div className="border border-gray-300 p-6 rounded-lg">
+        <div className="border p-6 rounded-lg">
           
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/graph.png"></img>
         </div>
       </div>
       <div className="xl:w-1/3 md:w-1/2 p-4">
-        <div className="border border-gray-300 p-6 rounded-lg">
-          
+        <div className="border p-6 rounded-lg">
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/safe.png"></img>
         </div>
       </div>
+      <div className="xl:w-1/3 md:w-1/2 p-4">
+        <div className="border p-6 rounded-lg">
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/nouns.png"></img>
+        </div>
+      </div>
+      {/* <div className="xl:w-1/3 md:w-1/2 p-4">
+        <div className="border p-6 rounded-lg">
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/mantle.png"></img>
+        </div>
+      </div>
+      <div className="xl:w-1/3 md:w-1/2 p-4">
+        <div className="border p-6 rounded-lg">
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/optimism.png"></img>
+        </div>
+      </div>
+      <div className="xl:w-1/3 md:w-1/2 p-4">
+        <div className="border p-6 rounded-lg">
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/gnosis.png"></img>
+        </div>
+      </div>
+      <div className="xl:w-1/3 md:w-1/2 p-4">
+        <div className="border p-6 rounded-lg">
+        <img alt="team" className=" object-cover object-center flex-shrink-0 mr-4" src="images/linea.png"></img>
+        </div>
+      </div>
+      <div className="xl:w-1/3 md:w-1/2 p-4">
+        <div className="border p-6 rounded-lg">
+        <img alt="team" className=" w-20 object-cover object-center flex-shrink-0 mr-4" src="images/fevm.svg"></img>
+        </div>
+      </div> */}
     </div>
-    <button className="flex mx-auto mt-16 text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
   </div>
 </section>
 <section className="text-gray-700 body-font border-t border-gray-200">
@@ -246,22 +264,12 @@ function Home() {
     <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
       <div className="flex flex-col mb-10 lg:items-start items-center">
         <div className="flex-grow">
-          <h2 className="text-gray-900 text-5xl title-font font-medium mb-3">🌿 Lens Protocol Native 🌿</h2>
-          <p className="leading-relaxed text-base">Right after being hired for a sponsorship gig, InfluLENSers can immediatley post on Lens - all from their influencer dashboard.</p>
-          <a className="mt-3 text-green-500 inline-flex items-center">Learn About Lens
-          </a>
+          <h2 className="text-gray-900 text-5xl title-font font-medium mb-3">View on Github</h2>
+          <p className="leading-relaxed text-base">Our code is fully open-source and we'd love your feedback! Find us on Github.</p>
+          <button className="flex mx-auto mt-16 text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Let's Go</button>
         </div>
       </div>
-      <div className="flex flex-col mb-10 lg:items-start items-center">
-
-        <div className="flex-grow">
-          <h2 className="text-gray-900 text-lg title-font font-medium mb-3">🍀 Hiring Powered by TalentLayer 🦝</h2>
-          <p className="leading-relaxed text-base">Your influencer marketing posts are broadcast to TalentLayer's network of interoperable hiring platforms.</p>
-          <a className="mt-3 text-green-500 inline-flex items-center">Learn About TalentLayer
-          </a>
-        </div>
-      </div>
-    </div>
+    </div>P
   </div>
 </section>
 <a href="https://github.com/InfluLenser" className="rounded-full w-12 h-12 bg-gray-100 fixed bottom-0 right-0 flex items-center justify-center text-gray-800 mr-8 mb-8 shadow-sm border-gray-300 border" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
