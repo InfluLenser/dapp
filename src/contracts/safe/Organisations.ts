44ae2affe6cf16b8d551cbaf85db75fb0c800c33
Quentin D.C <quent043@hotmail.com>
2023-05-13 09:56:30 +0100

    Added safe transactions methods

--- src/contracts/safe/Organisations.ts
+++ src/contracts/safe/Organisations.ts
@@ -1,5 +1,7 @@
-import { EthersAdapter, SafeAccountConfig, SafeFactory } from '@safe-global/protocol-kit';
+import SafeApiKit from '@safe-global/api-kit';
+import Safe, { EthersAdapter, SafeAccountConfig, SafeFactory } from '@safe-global/protocol-kit';
 import { ethers } from 'ethers';
+import { OperationType, SafeTransactionDataPartial } from '@safe-global/safe-core-sdk-types';
 
 // This file can be used to play around with the Safe Core SDK
 
@@ -72,4 +74,57 @@ export async function deploySafe(
 
   console.log('Deployed Safe:', safe.getAddress());
   return safe.getAddress();
+}
+
+export async function proposeSafeTransaction(
+  safeAddress: string,
+  toAddress: string,
+  signer: ethers.Signer,
+  value = '0',
+  data = '',
+): Promise<string | undefined> {
+  if (!signer) return;
+
+  // Create EthAdapter instance
+  const ethAdapter = new EthersAdapter({
+    ethers,
+    signerOrProvider: signer,
+  });
+
+  // Create Safe instance
+  const safe = await Safe.create({
+    ethAdapter,
+    safeAddress: safeAddress,
+  });
+
+  // Create Safe API Kit instance
+  const service = new SafeApiKit({
+    txServiceUrl: process.env.NEXT_PUBLIC_SAFE_API_URL as string,
+    ethAdapter,
+  });
+
+  // Create transaction
+  const safeTransactionData: SafeTransactionDataPartial = {
+    to: toAddress,
+    value: value, // 1 wei
+    data: data,
+    operation: OperationType.Call,
+  };
+
+  const safeTransaction = await safe.createTransaction({ safeTransactionData });
+
+  const senderAddress = await signer.getAddress();
+  const safeTxHash = await safe.getTransactionHash(safeTransaction);
+  const signature = await safe.signTransactionHash(safeTxHash);
+
+  // Propose transaction to the service
+  await service.proposeTransaction({
+    safeAddress: safeAddress,
+    safeTransactionData: safeTransaction.data,
+    safeTxHash,
+    senderAddress,
+    senderSignature: signature.data,
+  });
+
+  return safeTxHash;
 }
