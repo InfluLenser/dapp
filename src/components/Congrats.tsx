56e0c11fa5beabae86d3849fd44f0542eb42c7eb
Quentin D.C <quent043@hotmail.com>
2023-05-13 21:57:34 +0100

    First draft confirm page

---
+++ src/components/Congrats.tsx
@@ -0,0 +1,14 @@
+function Congrats() {
+  return (
+    <div className='max-w-7xl mx-auto text-center text-gray-900 sm:px-4 lg:px-0'>
+      <p className='text-5xl font-medium tracking-wider mb-8'>
+        Create{' '}
+        <span className='bg-clip-text text-transparent bg-gradient-to-r from-il-green-700 to-il-green-600'>
+          an organization
+        </span>
+      </p>
+    </div>
+  );
+}
+
+export default Congrats;
